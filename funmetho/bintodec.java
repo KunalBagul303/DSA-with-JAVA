3b2cbdab775cd3fa5c0110b825c8c330d1ff36fd
KunalBagul303 <kunalbagul315@gmail.com>
2026-01-02 23:45:28 +0530

    new chnage

---
+++ funmetho/bintodec.java
@@ -0,0 +1,21 @@
+public class bintodec {
+    public static void bitode(int binum){
+        int mynum = binum;
+        int pow = 0;
+        int decnum = 0;
+
+        while(binum>0){
+            int lastdigit = binum% 10;
+            decnum=decnum+(lastdigit*(int)Math.pow(2,pow));
+
+            pow++;
+
+            binum=binum/10;
+        }
+        System.out.println("decimal of"+ mynum + "="+decnum);
+    }
+
+    public static void main(String[] args) {
+        bitode(101);
+    }
+}
