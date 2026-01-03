b2439b19fdabf268eb6a8e920ca0ac7d81c6c534
KunalBagul303 <kunalbagul315@gmail.com>
2026-01-03 22:55:59 +0530

    2nd commit

---
+++ pattern-two/hollroh.java
@@ -0,0 +1,24 @@
+public class hollroh {
+    public static void hollow_rho(int n ){
+        for(int i=1; i<=n; i++){
+            //space
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //hollow rectangle - stars
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_rho(5);
+    }
+}
