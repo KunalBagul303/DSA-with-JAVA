f7907093d2e0ce0dda95ac65c8204b1aae6adce3
KunalBagul303 <kunalbagul315@gmail.com>
2026-01-16 22:07:57 +0530

    16 jan 2026

---
+++ String/palindorm.java
@@ -0,0 +1,23 @@
+
+
+public class palindorm {
+
+    public static boolean isPalindrom(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                //not a palidron
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrom(str));
+
+    }
+}
---
+++ String/teststring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class teststring {
+    public static void main(String[] args) {
+
+        String str = "abcd";
+        String str2 = new String("xyz0");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println(name);
+    }
+}
