0ce77c06bd0134b44bb4b974f1106bd4f6408a39
KunalBagul303 <kunalbagul315@gmail.com>
2026-01-25 16:01:06 +0530

    commit the change

---
+++ Recursion code/numsum.java
@@ -0,0 +1,17 @@
+
+public class numsum {
+
+    public static int calcSum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int sum1 = calcSum(n - 1);
+        int sum = n + sum1;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(calcSum(n));
+    }
+}
