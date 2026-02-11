c65bb3a618be5d63b2df48693e1e90e83c579df9
KunalBagul303 <kunalbagul315@gmail.com>
2026-02-11 18:31:25 +0530

    11 feb 2026

---
+++ pratice que/let75.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class let75 {
+
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else {   // nums[mid] == 2
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        System.out.println("Before Sorting: " + Arrays.toString(nums));
+
+        sortColors(nums);
+
+        System.out.println("After Sorting:  " + Arrays.toString(nums));
+    }
+}
