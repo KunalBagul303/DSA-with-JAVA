579067400cdce68e794b52e899c0cea3bb2e9bfe
KunalBagul303 <kunalbagul315@gmail.com>
2026-02-16 23:00:15 +0530

    16 feb 2026

---
+++ pratice que/let66.java
@@ -0,0 +1,31 @@
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+
+        int[] digits = {9, 9, 9};
+
+        int[] result = plusOne(digits);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
