aa72448210305dd264a8326209f70d8cb67c525e
KunalBagul303 <kunalbagul315@gmail.com>
2026-02-26 22:50:51 +0530

    26 feb 2026

---
+++ pratice que/tentooneee.java
@@ -0,0 +1,19 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class tentooneee {
+   public tentooneee() {
+   }
+
+   public static void Decending(int var0) {
+      if (var0 == 1) {
+         System.out.println(1);
+      } else {
+         System.out.println("" + var0 + " ");
+         Decending(var0 - 1);
+      }
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 10;
+      Decending(var1);
+   }
+}
