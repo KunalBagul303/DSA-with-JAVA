a7ca787b429524c789ef266653b66ace1bd36cdb
KunalBagul303 <kunalbagul315@gmail.com>
2026-03-09 23:32:47 +0530

    9 mar 2026

--- pratice que/onerec.java
+++ pratice que/onerec.java
@@ -5,14 +5,13 @@ public class onerec {
         if(n == 1){
             System.out.print(n);
             return;
-        }
-        System.out.println(n);
+9 mar 2026        System.out.println(n);
         onerecarsion(n-1);
     }
 
 
     public static void main(String[] args) {
-        int n  = 9;
+        int n  = 8;
         onerecarsion(n);
     }
 }
