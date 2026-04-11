e002a506a0aac4f1eb034f6bc9d2d4b7a64490d4
KunalBagul303 <kunalbagul315@gmail.com>
2026-04-11 22:09:38 +0530

    11 april 2026

--- Sorting algo/bubble.java
+++ Sorting algo/bubble.java
@@ -21,7 +21,7 @@ public class bubble {
     }
 
     public static void main(String[] args) {
-        int arr[] = {9,1,8,6,7,5,3,1};
+        int arr[] = {9,1,8,6,7,1};
         BubbleSort(arr);
         printarr(arr);
     }
