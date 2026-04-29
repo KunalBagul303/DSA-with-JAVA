17cbfb349ebf4effec4fb0afabb8300ba8586016
KunalBagul303 <kunalbagul315@gmail.com>
2026-04-29 23:51:32 +0530

    15 may 2026

--- pratice que/sorted.java
+++ pratice que/sorted.java
@@ -12,7 +12,7 @@ public class sorted {
         return isSorted(arr, i+1);
     }
     public static void main(String[] args) {
-        int arr[] = {5,5,6,6};
+        int arr[] = {5,6};
         System.out.println(isSorted(arr, 0));
     }
 }
