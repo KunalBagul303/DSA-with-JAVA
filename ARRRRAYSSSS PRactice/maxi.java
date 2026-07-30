8331fc1e9f50714371eb84552a2018753abe87f0
KunalBagul303 <kunalbagul315@gmail.com>
2026-07-30 10:49:36 +0530

    new zwrone que

--- ARRRRAYSSSS PRactice/maxi.java
+++ ARRRRAYSSSS PRactice/maxi.java
@@ -1,27 +1,60 @@
 public class maxi {
 
+    public static int[] getnepos(int[] arr) {
+        int negative = 0;
+        int postive = 0;
 
-        public static int maxi(int[] arr) {
-           int maxi = arr[0];
-           for(int i=0; i<arr.length; i++){
-            if(arr[i]>maxi){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                postive = postive + arr[i];
+            } else {
+                negative = negative + arr[i];
+            }
+        }
+        int ans[] = { postive, negative };
+        return ans;
+    }
+
+
+
+
+    public static int[] zeroone(int[] arr) {
+        int zero = 0;
+        int one = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                zero++;
+            } else {
+                one--;
+            }
+        }
+        int ans[] = { zero, one };
+        return ans;
+    }
+
+
+
+    public static int maxi(int[] arr) {
+        int maxi = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxi) {
                 maxi = arr[i];
             }
-           }
-           return maxi;
         }
-
-
-
-
+        return maxi;
+    }
 
     public static void main(String[] args) {
-        int[]  arr = {2,3,4,5,6,7,8,9};
-       
-         int ans = maxi(arr);
-         System.out.println(ans);
+        int[] arr = { 1,1,0,0,0,1,1,0,0,1,0 };
+
+        // int ans = maxi(arr);
+        // System.out.println(ans);
+
+        int[] ans = zeroone(arr);
+        System.out.println("zero sum = " + ans[0]);
+        System.out.println("one sum = " + ans[1]);
 
     }
 
 }
-
