5dbc2a520dbca17ba363034718095dda04f7fe3f
KunalBagul303 <kunalbagul315@gmail.com>
2026-07-31 10:46:40 +0530

    alternate

---
+++ ARRRRAYSSSS PRactice/alternate.java
@@ -0,0 +1,30 @@
+public class alternate {
+
+
+    public static void alternateee(int arr[]){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+
+        while(i<=j){
+            if(i == j){
+                System.out.println(arr[i]);
+                return;
+            }
+            else {
+                System.out.println(arr[i]);
+                i++;
+                System.out.println(arr[j]);
+                j--;
+            }
+        }
+        
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+          alternateee(arr);
+       
+    }
+}
